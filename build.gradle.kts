735ca4e9c4a3793caf236da949adf6ca09c72264
Oleg <37769766+TheDearbear@users.noreply.github.com>
2024-06-05 23:54:28 +0300

    Dumper implemented

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,35 @@
 plugins {
+    application
     kotlin("jvm") version "1.9.23"
 }
 
 group = "com.thedearbear"
 version = "1.0-SNAPSHOT"
 
+application {
+    mainClass = "com.thedearbear.MainKt"
+}
+
 repositories {
     mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    google()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
+    implementation("io.github.skylot:jadx-core:1.5.0-SNAPSHOT") {
+        isChanging = true
+    }
 
-tasks.test {
-    useJUnitPlatform()
+    implementation("io.github.skylot:jadx-dex-input:1.5.0-SNAPSHOT") {
+        isChanging = true
+    }
+
+    implementation("io.github.skylot:jadx-smali-input:1.5.0-SNAPSHOT") {
+        isChanging = true
+    }
+
+    implementation("org.slf4j:slf4j-simple:2.0.13")
 }
 kotlin {
     jvmToolchain(21)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,74 @@
 package com.thedearbear
 
-fun main() {
-    println("Hello World!")
+import jadx.api.*
+import jadx.core.dex.instructions.ConstStringNode
+import jadx.core.dex.instructions.InsnType
+import java.io.File
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        error("Please provide file!")
+    } else if (args.size > 1) {
+        error("Too many arguments")
+    }
+
+    val jArgs = JadxArgs()
+    jArgs.isSkipResources = true
+    jArgs.decompilationMode = DecompilationMode.FALLBACK
+    jArgs.setInputFile(File(args[0]))
+
+    try {
+        val decompiler = JadxDecompiler(jArgs)
+        decompiler.load()
+
+        for (clazz: JavaClass in decompiler.classes) {
+            for (method: JavaMethod in clazz.methods) {
+                val devKey = extractDevKey(method)
+
+                if (devKey != null) {
+                    println("DevKey found: $devKey")
+                    println("Found in method ${method.fullName}")
+                    return
+                }
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+private fun extractDevKey(method: JavaMethod): String? {
+    if (method.arguments.size != 0 ||
+        method.isClassInit || method.isConstructor ||
+        !method.returnType.isObject) {
+        return null
+    }
+
+    val node = method.methodNode
+    node.reload()
+
+    if (node.instructions == null) {
+        return null
+    }
+
+    val instructions = node.instructions.filterNotNull()
+
+    if (instructions.size != 4) {
+        return null
+    }
+
+    var current = instructions[0]
+    if (current.type != InsnType.NEW_INSTANCE ||
+        !current.result.initType.isObject ||
+        current.result.initType.`object` != method.returnType.`object`) {
+        return null
+    }
+
+    current = instructions[1]
+
+    return if (current is ConstStringNode && current.string.matches(Regex("[0-9a-f]+"))) {
+        current.string
+    } else {
+        null
+    }
 }
